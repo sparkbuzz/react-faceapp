878bf82d7b507876e81fbd9661d105483544d373
Josef van Niekerk <josef.van.niekerk@gmail.com>
2021-02-02 23:55:21 +0100

    Readjust video on window resize

--- packages/react-faceoff/src/hooks/use-window-size/use-window-size.tsx
+++ packages/react-faceoff/src/hooks/use-window-size/use-window-size.tsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react';
+import { debounce } from 'debounce';
+
 type WindowSize = {
   height: number,
   width: number
 }
 
+const DEBOUNCE_TIMEOUT_MS = 200;
+
 const useWindowSize = (): WindowSize => {
-  return {
-    width: window.innerWidth,
-    height: window.innerHeight
-  }
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0
+  });
+
+  useEffect(() => {
+    const handleWindowResize = debounce(() => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    }, DEBOUNCE_TIMEOUT_MS);
+
+    window.addEventListener('resize', handleWindowResize);
+  }, []);
+
+  return windowSize;
 }
 
 export { useWindowSize };
